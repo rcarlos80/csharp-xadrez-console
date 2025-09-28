805e84db8dca1b1528335d01e2a73413839b983f
Carlos Rabelo <carlosrabelo.dev@gmail.com>
2025-09-28 01:29:54 -0300

    Criando a classe Posicao

--- Course45_xadrez/Program.cs
+++ Course45_xadrez/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Course45_xadrez
+﻿using System;
+using tabuleiro;
+
+namespace Course45_xadrez
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Posicao pos = new Posicao(1, 1);
+
+            Console.WriteLine(pos);
         }
     }
 }
